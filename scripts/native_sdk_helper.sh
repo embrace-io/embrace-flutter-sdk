2f2c4dc9f6b9da2df1d4f74c4821d44bac4af2c8
Vitaliy <vitaliyf@users.noreply.github.com>
2025-10-24 18:52:45 -0400

    Also set embrace-android-sdk version in embrace_platform_interface/lib/method_channel_embrace.dart

--- scripts/native_sdk_helper.sh
+++ scripts/native_sdk_helper.sh
@@ -36,6 +36,7 @@ if [[ $action == "set" ]]; then
   # Example: https://github.com/embrace-io/embrace-flutter-sdk/pull/84
   if [[ $platform == "android" ]]; then
     $SED -i "s/^emb_android_sdk=.*/emb_android_sdk=${version}/" embrace_android/android/gradle.properties
+    $SED -i "s/static const String minimumAndroidVersion = '.*'/static const String minimumAndroidVersion = '${version}'/" embrace_platform_interface/lib/method_channel_embrace.dart
   fi
 
   # Example: https://github.com/embrace-io/embrace-flutter-sdk/pull/83/files#diff-e5d0042a93a4077ef0e0faeab47e4ecdddbdae595a19d7804934b7db1c0e7164
