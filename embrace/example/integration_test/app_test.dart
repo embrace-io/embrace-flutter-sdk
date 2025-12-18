07441d678e9f3f9a8dda41c3b5ffd6a13ab8941b
Benjamin Bennett <benjabenn@gmail.com>
2025-12-18 08:34:27 -0800

    Update app_test.dart

--- embrace/example/integration_test/app_test.dart
+++ embrace/example/integration_test/app_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:embrace_example/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
