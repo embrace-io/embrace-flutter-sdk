da8182fed885928f8c3b1e58e3acc6d1d33cb8c0
Benjamin Bennett <ben.bennett@embrace.io>
2026-08-18 11:53:57 -0700

    feat(flutter): add slow frame detection via SchedulerBinding timings (#349)

--- embrace/lib/src/navigation_observer.dart
+++ embrace/lib/src/navigation_observer.dart
@@ -1,5 +1,6 @@
 import 'package:embrace/embrace.dart';
 import 'package:embrace/embrace_api.dart';
+import 'package:embrace/src/embrace_frame_detector.dart';
 import 'package:embrace/src/embrace_startup_tracker.dart';
 import 'package:embrace/src/pointer_input_tracker.dart';
 import 'package:flutter/material.dart';
@@ -171,6 +172,7 @@ class EmbraceNavigationObserver extends RouteObserver<ModalRoute<dynamic>> {
       final name = settings?.name;
       if (name != null) {
         Embrace.instance.startView(name);
+        updateCurrentRoute(name);
       }
     }
   }
